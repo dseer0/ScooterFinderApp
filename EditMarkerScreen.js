ce825e1d53d94e7e94440652e6ed3f97673ed066
dseer0 <dseerFFFF@gmail.com>
2021-12-10 15:27:56 +0100

    edit marker screen

--- EditMarkerScreen.js
+++ EditMarkerScreen.js
@@ -21,40 +21,71 @@ import {
   Image,
   ImageBackground,
   Button,
+  TouchableOpacity,
 } from 'react-native';
 import MapView from 'react-native-maps';
 
-const EditMarkerScreen = navigation => {
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-    },
-    delete: {},
-  });
-
-  return (
-    <View style={styles.container}>
-      <View
-        style={{
-          borderLeftWidth: 4,
-          borderRightWidth: 4,
-          height: 70,
-        }}>
-        <TextInput
-          multiline
-          caretHidden
-          style={{
-            height: 70,
-            backgroundColor: '#ffffff',
-            paddingLeft: 15,
-            paddingRight: 15,
-          }}
-        />
-      </View>
-      <Button color="#ff5c5c" title="DELETE" onPress={() => {}} />
-      <Button title="EDIT" onPress={() => {}} />
+const EditMarkerScreen = ({navigation}) => (
+  <View style={styles.container}>
+    <View style={styles.markerForm}>
+      <Text style={styles.header}>Edit Marker</Text>
+      <TextInput
+        style={styles.textinput}
+        placeholder="description"
+        underlineColorAndroid={'transparent'}
+      />
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate('MapScreen')}>
+        <Text style={styles.buttontxt}>Submit</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate('MapScreen')}>
+        <Text style={styles.buttontxt}>Delete</Text>
+      </TouchableOpacity>
     </View>
-  );
-};
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingLeft: 60,
+    paddingRight: 60,
+  },
+  markerForm: {
+    alignSelf: 'stretch',
+  },
+  header: {
+    fontSize: 24,
+    color: '#000',
+    paddingBottom: 10,
+    marginBottom: 40,
+    borderBottomColor: '#000',
+    borderBottomWidth: 2,
+  },
+  textinput: {
+    alignSelf: 'stretch',
+    height: 40,
+    marginBottom: 30,
+    color: '#000',
+    borderBottomColor: '#1b4798',
+    borderBottomWidth: 2,
+  },
+  button: {
+    alignSelf: 'stretch',
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: '#1b4798',
+    marginTop: 30,
+  },
+  buttontxt: {
+    fontSize: 18,
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+});
 
 export default EditMarkerScreen;
